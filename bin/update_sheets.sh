d95dba83d04f32a75f8f2f181af9ecf61d6dc1f7
apross <a.pross@impulse-info.com>
2017-03-30 14:05:22 +0200

    add_bin

--- bin/update_sheets.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#set -x
-rm /home/apross/Desktop/7??2.xlsx
-rm '/home/apross/Desktop/migratie voortgang.xlsx'
-echo copy sheets
-cd '/work/impulse_teksten/3. afdelingen/service/Helpdesk/USER/Update verzoeken (HF-PATCH-VERSIE)/' && tar -cpf - 7???.xlsx | ( cd /home/apross/Desktop/ && tar -xvpf - )
-cd '/home/apross/Desktop/work/impulse_teksten/3. afdelingen/service/Systeembeheer/implementaties/linux6/' && tar -cpf - 'migratie voortgang.xlsx' | ( cd /home/apross/Desktop/ && tar -xvpf - )
